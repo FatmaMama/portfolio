725fe8a47509d35717a9b66dd5dd66453330a947
FatmaMama <fatma_mama@hotmail.fr>
2021-11-03 01:43:17 +0100

    fix thank page route

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import Header from './layout/Header';
 import Footer from './layout/Footer';
-import Home from './screens/Home'
+import Home from './screens/Home';
 import ProjectDetails from './screens/ProjectDetails';
+import Thank from './screens/Thank'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
                 <Switch>
                   <Route exact path="/" component={Home} />
                   <Route exact path="/project/:id" component={ProjectDetails} />
+                  <Route exact path="/thank" component={Thank} />
                 </Switch>
             <Footer />
           </div>
