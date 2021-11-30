4f2bd41393f847664acf37cc5cf0082056ea1062
FatmaMama <fatma_mama@hotmail.fr>
2021-11-30 01:10:25 +0100

    add typing text

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react';
+import { useEffect, useRef } from 'react';
 import data from '../data/data.json'
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Technologies from '../components/Technologies';
 import Contact from '../components/Contact';
+import Typical from 'react-typical';
+
 
 export default function Home() {
+
     return (
         <Fragment>
             <section className="home pt-md-5 pt-1" id="home">
@@ -16,7 +20,24 @@ export default function Home() {
                             <h3 className="text-light pt-5">Hello, my name is</h3>
                             <h1 className="text-light fw-bold my-name" >Fatma Mama</h1>
                             <h3 className="text-light">And I am a {' '} 
-                                <span className="pink">Junior full-stack JS Developer</span>
+                                {/* <span className="pink">Junior full-stack JS Developer</span> */}
+                                <Typical
+                                    className="pink"
+                                    steps={[
+                                            'Junior full-stack JS Developer',
+                                        1000,
+                                        '',
+                                        500,
+                                        'Junior full-stack JS Developer',
+                                        1000,
+                                        '',
+                                        500,
+                                        'Junior full-stack JS Developer',
+                                        1000
+                                        ]}
+                                        wrapper="span"
+                                        loop={Infinity}
+                                />
                             </h3>
                         </div>
 
